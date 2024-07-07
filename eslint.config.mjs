2e9beb27248c07e71d342a8e10f725baa050d899
Elecshen <elecshen@yandex.ru>
2024-07-07 19:42:27 +0600

    orginized linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugin: {
+      eslintPluginPrettier,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
   { languageOptions: { ...globals.browser, ...globals.node } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
 ];
